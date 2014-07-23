32da456b80fa01781232253329cd1664941c6b91
AlexAbraham1 <alex@abraham.net>
2014-07-23 17:16:04 -0400

    Some refactoring

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -26,15 +26,10 @@ void ofApp::setup() {
 	haarFinder.setNeighbors(1);
 	haarFinder.setScaleHaar(2);
 
-	one_second_time = ofGetSystemTime();
-	camera_fps = 0;
-	frames_one_sec = 0;
-
 	firstRun = true;
-	invertColors = false;
 
 	minRadius = 10;
-	maxRadius = 30;
+	maxRadius = 50;
 
 	maxTimesDrawn = 20;
 }
@@ -43,19 +38,13 @@ void ofApp::setup() {
 void ofApp::update() {
 	grabber.update();
 	if (grabber.isFrameNew()) {
-		frames_one_sec++;
-		if (ofGetSystemTime() - one_second_time >= 1000) {
-			camera_fps = frames_one_sec;
-			frames_one_sec = 0;
-			one_second_time = ofGetSystemTime();
-		}
 
 		//Detect Faces
 		colorCv = grabber.getPixels();
 		colorCvSmall.scaleIntoMe(colorCv, CV_INTER_NN);
 		grayCv = colorCvSmall;
 		haarFinder.findHaarObjects(grayCv);
-		right_eyes = haarFinder.blobs;
+		eyes = haarFinder.blobs;
 
 		if (firstRun) {
 			int x = 0;
@@ -85,8 +74,14 @@ void ofApp::update() {
 
 			}
 			firstRun = false;
-			//setZIndex();
+			fixZIndex();
 			cleanupCircles();
+
+			for(std::list<Circle*>::iterator circle = added_circles.begin(); circle != added_circles.end(); ++circle) {
+				if ((*circle)->timesDrawn > maxTimesDrawn) {
+					circle = added_circles.erase(circle);
+				}
+			}
 
 		}
 
@@ -99,35 +94,27 @@ void ofApp::draw() {
 	for (std::vector<Circle>::size_type i = 0; i != circles.size(); i++) {
 		Circle * circle = circles[i];
 		ofColor color = grabber.getPixelsRef().getColor(circle->x, circle->y);
-		if (invertColors) {
-			color.invert();
-		}
 		circle->color = color;
 		circle->drawCircle();
 	}
 
-	for (std::vector<Circle>::size_type i = 0; i != added_circles.size(); i++) {
-		Circle * circle = added_circles[i];
-
-		if (circle->timesDrawn <= maxTimesDrawn) {
-			circle->drawCircle();
+	for(std::list<Circle*>::iterator circle = added_circles.begin(); circle != added_circles.end(); ++circle) {
+		if ((*circle)->timesDrawn <= maxTimesDrawn) {
+			(*circle)->drawCircle();
 		}
 	}
 
 
 	ofPushStyle();
 
-	for (std::vector<ofxCvBlob>::size_type i = 0; i != right_eyes.size(); i++) {
-		ofxCvBlob& eye = right_eyes[i];
+	for (std::vector<ofxCvBlob>::size_type i = 0; i != eyes.size(); i++) {
+		ofxCvBlob& eye = eyes[i];
 		int x = (eye.boundingRect.x + (eye.boundingRect.width/2)) * haarScaleFactor;
 		int y = (eye.boundingRect.y + (eye.boundingRect.height/2)) * haarScaleFactor;
 		int radius1 = eye.boundingRect.width;
-		int radius2 = eye.boundingRect.width / 2;
 
-		ofSetColor(255, 255, 255);
-		ofCircle(x, y, radius1);
-		ofSetColor(0, 0, 0);
-		ofCircle(x, y, radius2);
+		ofSetColor(0, 0, 255);
+		ofCircle(x, y, 100, radius1);
 	}
 	ofPopStyle();
 }
@@ -224,27 +211,16 @@ void ofApp::cleanupCircles() {
 	ofLog() << "NUMBER OF CIRCLES: " + ss.str();
 }
 
-bool ofApp::removeCircles(int x1, int y1, int x2, int y2, int radius1,
-		int radius2) {
-	float distance = ofDist(x1, y1, x2, y2);
-	int radius = radius1 + radius2;
-
-	if (radius >= distance) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
 bool ofApp::isBlankSpace(int x, int y) {
 	for (std::vector<Circle>::size_type i = 0; i != circles.size(); i++) {
+
 		Circle * circle = circles[i];
 
 		float x2 = pow((circle->x - x), 2);
 		float y2 = pow((circle->y - y), 2);
 		float radius2 = pow((circle->radius), 2);
 
-		if ((x2 + y2) < (radius2)) {
+		if ((x2 + y2) < (radius2)/2) {
 			return false;
 		}
 
@@ -266,21 +242,14 @@ void ofApp::switchCamera() {
 	grabber.initGrabber(screenWidth, screenHeight);
 }
 
-void ofApp::switchInvert() {
-	invertColors = !invertColors;
-}
-
-void ofApp::setZIndex() {
+void ofApp::fixZIndex() {
 	for (std::vector<Circle>::size_type i = 0; i != circles.size(); i++) {
 		Circle * circle = circles[i];
 
-		int zIndex = int(
-				0
-						+ static_cast<float>(rand())
-								/ (static_cast<float>(RAND_MAX
-										/ (circles.size() - 0)))) % 200;
-
-		circle->z = zIndex;
+		float smallRadius = (minRadius + ((minRadius + maxRadius)/2))/2;
+		if (circle->radius <= smallRadius) {
+			circle->z = 50;
+		}
 	}
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -37,44 +37,34 @@ public:
 	void cancelPressed();
 
 	void cleanupCircles();
-	bool removeCircles(int x1, int y1, int x2, int y2, int radius1,
-			int radius2);
 	bool isBlankSpace(int x, int y);
 	void switchCamera();
 	void switchInvert();
-	void setZIndex();
+	void fixZIndex();
 	void saveImage();
 	void touchCircle(int x, int y);
+	void detectHandCircles(int xLow, int yLow, int xHigh, int yHigh);
 
 	ofVideoGrabber grabber;
 
 	ofxCvColorImage colorCv;
 	ofxCvColorImage colorCvSmall;
 	ofxCvGrayscaleImage grayCv;
-
 	ofxCvHaarFinder haarFinder;
+	float haarScaleFactor;
 
 	bool firstRun;
 	bool invertColors;
 
-	int *circleArray;
-	float haarScaleFactor;
+
 
 	std::vector<Circle*> circles;
-	std::vector<ofxCvBlob> right_eyes;
-	std::vector<ofxCvBlob> left_eyes;
+	std::vector<ofxCvBlob> eyes;
 
-	std::vector<Circle*> added_circles;
+	std::list<Circle*> added_circles;
 
-	ofImage googleEye;
-
-	int one_second_time;
-	int camera_fps;
-	int frames_one_sec;
 	int minRadius;
 	int maxRadius;
-	int minZ;
-	int maxZ;
 	int maxTimesDrawn;
 
 	int DEVICE_ID;
